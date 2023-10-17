005ab79a67d9e8fd6f316159d8e361d536c5d21d
Fyodor Amanov <127713333+FyodorAmanov1@users.noreply.github.com>
2023-10-17 23:38:18 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,23 @@
 /*
- * Author:
+ * Author: Fyodor Amanov
  * Date:
  * Name:
  */
+#include <iostream>
+#include <string>
+using namespace std;
+class plant{
+private:
+    int number_of_leaves;
+public:
+    string name;
+    int SetName(string NewName){
+        name = NewName;
+        return 0;
+    }
+    void PrintName(){
+        cout << name << endl;
+    }
+    bool With_flowers;
+    string whose;
+};
